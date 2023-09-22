ac1dab4e0cfe3bddc4b18b8a2342e4e00c90648f
Adam Silver <adambsilver@gmail.com>
2023-09-22 13:08:45 +0100

    Add country autocomplete

--- app/routes.js
+++ app/routes.js
@@ -13,3 +13,7 @@ router.post('/new/email-address', (req, res) => {
 router.post('/new/code', (req, res) => {
   res.redirect('/new/name')
 })
+
+router.post('/new/name', (req, res) => {
+  res.redirect('/new/country')
+})
