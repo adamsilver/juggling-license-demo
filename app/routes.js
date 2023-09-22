277056271e0384078c53111bad002fb92f0994da
Adam Silver <adambsilver@gmail.com>
2023-09-22 12:39:55 +0100

    Revert

--- app/routes.js
+++ app/routes.js
@@ -9,7 +9,3 @@ const router = govukPrototypeKit.requests.setupRouter()
 router.post('/new/email-address', (req, res) => {
   res.redirect('/new/code')
 })
-
-router.post('/new/code', (req, res) => {
-  res.redirect('/new/name')
-})
